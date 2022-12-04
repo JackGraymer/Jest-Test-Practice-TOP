5a41dae0d4cd3defa78ffcae8daa90543d1af720
Jack Graymer <jackgraymer@gmail.com>
2022-12-04 15:27:27 +0100

    add calculator object with basic functions and test

--- assignment.js
+++ assignment.js
@@ -10,9 +10,27 @@ function reverse(sentence){
     return sentence
 }
 
+const calculator = {
+    add(a, b) {
+        return a+b
+    },
+
+    substract(a, b){
+        return a-b
+    },
+
+    multiply(a, b) {
+        return a*b
+    },
+
+    divide(a, b){
+        return a/b
+    }
+}
 
 
 export {
     capitalize,
-    reverse
+    reverse,
+    calculator
 }
--- assignment.test.js
+++ assignment.test.js
@@ -1,4 +1,4 @@
-import {capitalize, reverse} from './assignment'
+import {capitalize, reverse, calculator} from './assignment'
 
 test('Capitalize first letter of string', () => {
     expect(capitalize('sentence')).toBe('Sentence')
@@ -7,4 +7,11 @@ test('Capitalize first letter of string', () => {
 test('Reverse sentence', () => {
     expect (reverse('1357')).toBe('7531')
     expect (reverse('Play Lego')).toBe('ogeL yalP')
+})
+
+test('Calculator with add, substract, divide and multiply', () => {
+    expect (calculator.add( 3, 5 )).toBe(8)
+    expect (calculator.substract(300, 150)).toBe(150)
+    expect (calculator.multiply(4, 5)).toBe(20)
+    expect (calculator.divide(200, 5)).toBe(40)
 })
